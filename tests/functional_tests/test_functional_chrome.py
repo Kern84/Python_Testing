ce35a6f530a5a9e814ebc5266b245644938c860b
Kern84 <adrienleroy1984@gmail.com>
2022-04-06 14:42:17 +0200

    tests finished

--- tests/functional_tests/test_functional_chrome.py
+++ tests/functional_tests/test_functional_chrome.py
@@ -7,28 +7,67 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class TestUserExperience:
-    def test_login_chrome_sucess(self):
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        driver.maximize_window()
-        driver.get("http://127.0.0.1:5000/")
-        assert "GUDLFT" in driver.title
-        driver.find_element(By.NAME, "email").send_keys(
-            "john@simplylift.co" + Keys.RETURN
-        )
-        WebDriverWait(driver, timeout=1).until(EC.title_contains("Summary"))
-        assert "Summary" in driver.title
-        driver.quit()  # no because of succession off actions
+def test_user_experience_chrome(mock_modified_competitions_date):
+    """
+    Function to test the user experience.
+    Go to the site index page;
+    Click on the link to go to the club points page (user not logged in);
+    Return to the index page;
+    Try to log in with a wrong email;
+    Log in with a correct email;
+    Click on the link to go to the club points page (user logged in);
+    Return to the showSummary page;
+    Click on the link to go to the booking page;
+    Book 4 places for the competition;
+    Logout.
+    """
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.maximize_window()
+    driver.get("http://127.0.0.1:5000/")
+    WebDriverWait(driver, timeout=1).until(EC.title_contains("GUDLFT"))
+    assert "GUDLFT" in driver.title
 
-    def test_login_chrome_wrong_email_should_fail(self):
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        driver.maximize_window()
-        driver.get("http://127.0.0.1:5000/")
-        assert "GUDLFT" in driver.title
-        driver.find_element(By.NAME, "email").send_keys("wrong@email.fr" + Keys.RETURN)
-        WebDriverWait(driver, timeout=1).until(EC.title_contains("GUDLFT"))
-        assert "Sorry, that email wasn't found." in driver.page_source
-        driver.quit()  # no because of succession off actions
+    club_points_link = driver.find_element(By.LINK_TEXT, "See clubs")
+    club_points_link.click()
+    WebDriverWait(driver, timeout=1).until(EC.title_contains("Club Points"))
+    assert "Club Points" in driver.title
 
+    driver.back()
+    WebDriverWait(driver, timeout=1).until(EC.title_contains("GUDLFT"))
+    assert "GUDLFT" in driver.title
 
-# TODO create a function for the user experience with every function needed
+    driver.find_element(By.NAME, "email").send_keys("wrong@email.fr" + Keys.RETURN)
+    WebDriverWait(driver, timeout=1).until(EC.title_contains("GUDLFT"))
+    assert "Sorry, that email wasn't found." in driver.page_source
+
+    driver.find_element(By.NAME, "email").send_keys("john@simplylift.co" + Keys.RETURN)
+    WebDriverWait(driver, timeout=1).until(EC.title_contains("Summary"))
+    assert "Summary" in driver.title
+
+    club_points_log_in_link = driver.find_element(By.LINK_TEXT, "See clubs")
+    club_points_log_in_link.click()
+    WebDriverWait(driver, timeout=1).until(EC.title_contains("Club Points"))
+    assert "Club Points" in driver.title
+
+    driver.back()
+    WebDriverWait(driver, timeout=1).until(EC.title_contains("Summary"))
+    assert "Summary" in driver.title
+
+    book_places_link = driver.find_element(By.LINK_TEXT, "Book Places")
+    book_places_link.click()
+    WebDriverWait(driver, timeout=1).until(EC.title_contains("Booking for"))
+    assert "Booking for" in driver.title
+
+    register_places = driver.find_element(By.NAME, "places")
+    register_places.send_keys("4")
+    validate_choice = driver.find_element(By.TAG_NAME, "button")
+    validate_choice.click()
+    WebDriverWait(driver, timeout=1).until(EC.title_contains("Summary"))
+    assert "Great-booking complete!" in driver.page_source
+
+    logout_link = driver.find_element(By.LINK_TEXT, "Logout")
+    logout_link.click()
+    WebDriverWait(driver, timeout=1).until(EC.title_contains("GUDLFT"))
+    assert "GUDLFT" in driver.title
+
+    driver.quit()
--- tests/unit_tests/test_route.py
+++ tests/unit_tests/test_route.py
@@ -42,8 +42,8 @@ class TestShowSummary:
         """
         response = client.post("/showSummary", data=dict(dict_wrong_email))
         data = response.data.decode()
-        assert response.status_code == 500  # should not crash, 200
-        # assert "Sorry, that email wasn't found." in data
+        assert response.status_code == 200
+        assert "Sorry, that email wasn't found." in data
 
     def test_login_wrong_input_should_fail(self, client):
         """
@@ -53,8 +53,8 @@ class TestShowSummary:
         """
         response = client.post("/showSummary", data=dict(dict_wrong_input))
         data = response.data.decode()
-        assert response.status_code == 500  # should not crash, 200
-        # assert "Sorry, that email wasn't found." in data
+        assert response.status_code == 200
+        assert "Sorry, that email wasn't found." in data
 
     def test_login_no_input_should_fail(self, client):
         """
@@ -64,8 +64,8 @@ class TestShowSummary:
         """
         response = client.post("/showSummary", data={"email": ""})
         data = response.data.decode()
-        assert response.status_code == 500  # should not crash, 200
-        # assert "Sorry, that email wasn't found." in data
+        assert response.status_code == 200
+        assert "Sorry, that email wasn't found." in data
 
     def test_get_show_summary_should_fail(self, client):
         """
@@ -74,7 +74,7 @@ class TestShowSummary:
         it should return a 405 HTTP code (Method Not Allowed).
         """
         response = client.get("/showSummary")
-        assert response.status_code == 405  # method should be POST
+        assert response.status_code == 405
 
 
 class TestBook:
@@ -100,8 +100,8 @@ class TestBook:
         """
         response = client.get("/book/WrongComp/WrongClub")
         data = response.data.decode()
-        assert response.status_code == 500
-        # assert "Something went wrong-please try again" in data
+        assert response.status_code == 200
+        assert "Something went wrong-please try again" in data
 
     def test_book_wrong_competition_path_should_fail(
         self, client, mock_modified_competitions_date
@@ -113,8 +113,8 @@ class TestBook:
         """
         response = client.get("/book/WrongComp/Simply%20Lift")
         data = response.data.decode()
-        assert response.status_code == 500
-        # assert "Something went wrong-please try again" in data
+        assert response.status_code == 200
+        assert "Something went wrong-please try again" in data
 
     def test_book_wrong_club_path_should_fail(
         self, client, mock_modified_competitions_date
@@ -126,8 +126,8 @@ class TestBook:
         """
         response = client.get("/book/Spring%20Festival/WrongClub")
         data = response.data.decode()
-        assert response.status_code == 500
-        # assert "Something went wrong-please try again" in data
+        assert response.status_code == 200
+        assert "Something went wrong-please try again" in data
 
     def test_book_full_competition_should_fail(
         self, client, mock_modified_full_competitions
@@ -140,7 +140,7 @@ class TestBook:
         response = client.get("/book/Spring%20Festival/Simply%20Lift")
         data = response.data.decode()
         assert response.status_code == 200
-        # assert "Something went wrong-please try again" in data
+        assert "Something went wrong-please try again" in data
 
     def test_book_root_path_should_fail(self, client):
         """
@@ -149,12 +149,12 @@ class TestBook:
         it should return a 404 HTTP code because the page doesn't exist.
         """
         response = client.get("/book/")
-        assert response.status_code == 404  # useful ?
+        assert response.status_code == 404
 
     def test_post_book_should_fail(self, client, mock_modified_competitions_date):
         """Test the book path, with POST, it should return a 405 HTTP code (Method Not Allowed)."""
         response = client.post("/book/Spring%20Festival/Simply%20Lift")
-        assert response.status_code == 405  # method should be GET
+        assert response.status_code == 405
 
     def test_book_past_competition_should_fail(self, client):
         """
@@ -164,8 +164,8 @@ class TestBook:
         """
         response = client.get("/book/Fall%20Classic/Simply%20Lift")
         data = response.data.decode()
-        assert response.status_code == 200  # should be 403
-        # assert "MESSAGE PAST COMPETITION" in data
+        assert response.status_code == 200
+        assert "You cannot book places for a past competition" in data
 
 
 class TestPurchasePlaces:
@@ -209,7 +209,9 @@ class TestPurchasePlaces:
         )
         data = response.data.decode()
         assert response.status_code == 200
-        # assert "MESSAGE ERROR" in data
+        assert (
+            "You must register a positive number of places (between 1 and 12)" in data
+        )
 
     def test_purchase_negative_number_of_places_should_fail(
         self, client, mock_modified_competitions_date
@@ -229,7 +231,9 @@ class TestPurchasePlaces:
         )
         data = response.data.decode()
         assert response.status_code == 200
-        # assert "MESSAGE ERROR" in data
+        assert (
+            "You must register a positive number of places (between 1 and 12)" in data
+        )
 
     def test_purchase_too_much_places_should_fail(
         self, client, mock_modified_competitions_date
@@ -249,7 +253,9 @@ class TestPurchasePlaces:
         )
         data = response.data.decode()
         assert response.status_code == 200
-        # assert "MESSAGE ERROR" in data
+        assert (
+            "You must register a positive number of places (between 1 and 12)" in data
+        )
 
     def test_purchase_places_with_not_enough_points_should_fail(
         self, client, mock_modified_competitions_date
@@ -269,7 +275,7 @@ class TestPurchasePlaces:
         )
         data = response.data.decode()
         assert response.status_code == 200
-        # assert "MESSAGE ERROR" in data
+        assert "You do not have enough points to make this booking" in data
 
     def test_purchase_places_should_reduce_club_points_success(
         self, client, mock_modified_competitions_date
@@ -277,7 +283,8 @@ class TestPurchasePlaces:
         """
         Test the purchasePlaces path,
         with POST and a correct club, competition and places,
-        it should return a 200 HTTP code (OK), the response should contain some text and the competition should have a reduction of it's available places.
+        it should return a 200 HTTP code (OK),
+        the response should contain some text and the competition should have a reduction of it's available places.
         """
         response = client.post(
             "/purchasePlaces",
@@ -290,7 +297,7 @@ class TestPurchasePlaces:
         expected_value = 10
         data = response.data.decode()
         assert response.status_code == 200
-        # assert "Points available: {}".format(expected_value) in data
+        assert "Points available: {}".format(expected_value) in data
 
     def test_purchase_places_should_reduce_competition_points_success(
         self, client, mock_modified_competitions_date
@@ -317,7 +324,7 @@ class TestPurchasePlaces:
     def test_get_purchase_should_fail(self, client):
         """Test the purchasePlaces path, with GET, it should return a 405 HTTP code (Method Not Allowed)."""
         response = client.get("/purchasePlaces")
-        assert response.status_code == 405  # because the method should be POST
+        assert response.status_code == 405
 
 
 class TestLogout:
@@ -361,10 +368,10 @@ class TestDisplayPoints:
         """
         response = client.get("/displayPoints")
         data = response.data.decode()
-        assert response.status_code == 404  # should be 200
-        # assert "Club list" in data
+        assert response.status_code == 200
+        assert "Club list" in data
 
     def test_post_display_clubs_points_should_fail(self, client):
         """Test the displayPopints path, with POST, it should return a 405 HTTP code (Method Not Allowed)."""
         response = client.post("/displayPoints")
-        assert response.status_code == 404  # should be 405
+        assert response.status_code == 405
